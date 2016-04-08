80d365f9d8170f19ec1523a1f2a571048e55805f
Aaron Peltz <aaron.peltz@gmail.com>
2016-04-07 23:22:30 -0700

    Added files via upload

--- content.js
+++ content.js
@@ -1,11 +1,8 @@
-// content.js
-console.log("I can be accessed from content.js");
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      if (request.instructions === "giveMeFirstImgPlease"){
-        var firstImgTag = 'http:'+$('body').find('img').attr('src').replace(/http:|https:/,'');
-        var messageBackToExtension = {fromTab: "imgTagAsRequested", imgTagSrc: firstImgTag};
-        sendResponse(messageBackToExtension);
-      }
-
+  if (request.instructions === "giveMeFirstImgPlease") {
+    var firstImgTag = 'http:' + $('body').find('img').attr('src').replace(/http:|https:/, '');
+    var msgBackToExt = {fromTab: "imgTagAsRequested", imgTagSrc: firstImgTag};
+    sendResponse(msgBackToExt);
+  }
 });
